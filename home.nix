a134deb276fa090e5db3a14b3e0ce78bca5bf81d
Christoph Blessing <chris24.blessing@gmail.com>
2024-03-28 16:34:47 +0100

    Configuer neovim

--- home.nix
+++ home.nix
@@ -17,121 +17,40 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    extraLuaConfig = ''
-      vim.g.mapleader = ' '
-      vim.g.maplocalleader = ' '
-      vim.opt.number = true
-      vim.opt.relativenumber = true
-      vim.opt.mouse = 'a'
-      vim.opt.showmode = false
-      vim.opt.clipboard = 'unnamedplus'
-      vim.opt.breakindent = true
-      vim.opt.undofile = true
-      vim.opt.ignorecase = true
-      vim.opt.smartcase = true
-      vim.opt.signcolumn = 'yes'
-      vim.opt.updatetime = 250
-      vim.opt.timeoutlen = 300
-      vim.opt.splitright = true
-      vim.opt.splitbelow = true
-      vim.opt.list = true
-      vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-      vim.opt.inccommand = 'split'
-      vim.opt.cursorline = true
-      vim.opt.scrolloff = 10
-
-      vim.opt.hlsearch = true
-      vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-      vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
-      vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-      vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-      vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-      vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
-      vim.api.nvim_create_autocmd('TextYankPost', {
-        desc = 'Highlight when yanking (copying) text',
-        group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-        callback = function()
-          vim.highlight.on_yank()
-        end,
-      })
-
-      require('Comment').setup()
-
-      require('gitsigns').setup({
-        signs = {
-          add = { text = '+' },
-          change = { text = '~' },
-          delete = { text = '_' },
-          topdelete = { text = '‾' },
-          changedelete = { text = '~' },
-        }
-      })
-
-      require('which-key').setup()
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
-
-      require('telescope').setup({
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
-      })
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>/', function()
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer' })
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = '[S]earch [/] in Open Files' })
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
-    '';
+    extraConfig = ":luafile neovim/init.lua";
     plugins = with pkgs.vimPlugins; [
       vim-sleuth
       comment-nvim
       gitsigns-nvim
       which-key-nvim
       telescope-nvim
+      nvim-lspconfig
+      fidget-nvim
+      neodev-nvim
+      nvim-cmp
+      luasnip
+      cmp_luasnip
+      cmp-nvim-lsp
+      cmp-path
+      tokyonight-nvim
+      conform-nvim
+      todo-comments-nvim
+      mini-nvim
+      nvim-treesitter.withAllGrammars
     ];
   };
+
   programs.nushell = {
     enable = true;
     configFile.source = nushell/config.nu;
     envFile.source = nushell/env.nu;
   };
-
+  
+  home.packages = with pkgs; [
+    lua-language-server
+    stylua
+    ripgrep
+  ];
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
