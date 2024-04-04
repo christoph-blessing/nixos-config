4290ad8c21655b68a2c2418c3897edad085025c5
Christoph Blessing <chris24.blessing@gmail.com>
2024-04-04 15:17:37 +0200

    Enable nerd fonts in neovim

--- neovim/init.lua
+++ neovim/init.lua
@@ -3,7 +3,7 @@ vim.cmd.hi("Comment gui=none")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
