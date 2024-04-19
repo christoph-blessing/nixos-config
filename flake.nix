6043e80518ce1cb7c502951f3635a9f9675584bf
Christoph Blessing <chris24.blessing@gmail.com>
2024-04-19 12:53:31 +0200

    Move home manager configuration

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 	{
 	  home-manager.useGlobalPkgs = true;
 	  home-manager.useUserPackages = true;
-	  home-manager.users.chris = import ./home.nix;
+	  home-manager.users.chris = import ./personal/home.nix;
 	}
       ];
     };
