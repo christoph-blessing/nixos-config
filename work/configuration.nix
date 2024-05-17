0f2bdcc5e90498a21c42b856631bbf45d2b03160
Christoph Blessing <chris24.blessing@gmail.com>
2024-05-17 12:31:46 +0200

    Fix webcam

--- work/configuration.nix
+++ work/configuration.nix
@@ -5,6 +5,29 @@
   ...
 }:
 
+let
+  ivsc-firmware =
+    with pkgs;
+    stdenv.mkDerivation rec {
+      pname = "ivsc-firmware";
+      version = "main";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "intel";
+        repo = "ivsc-firmware";
+        rev = "10c214fea5560060d387fbd2fb8a1af329cb6232";
+        sha256 = "sha256-kEoA0yeGXuuB+jlMIhNm+SBljH+Ru7zt3PzGb+EPBPw=";
+      };
+
+      installPhase = ''
+        mkdir -p $out/lib/firmware/vsc/soc_a1_prod
+
+        cp firmware/ivsc_pkg_ovti01a0_0.bin $out/lib/firmware/vsc/soc_a1_prod/ivsc_pkg_ovti01a0_0_a1_prod.bin
+        cp firmware/ivsc_skucfg_ovti01a0_0_1.bin $out/lib/firmware/vsc/soc_a1_prod/ivsc_skucfg_ovti01a0_0_1_a1_prod.bin
+        cp firmware/ivsc_fw.bin $out/lib/firmware/vsc/soc_a1_prod/ivsc_fw_a1_prod.bin
+      '';
+    };
+in
 {
   imports = [
     ./hardware-configuration.nix
@@ -136,7 +159,15 @@
     pulse.enable = true;
   };
 
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "zoom" ];
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "zoom"
+      "ipu6-camera-bins-unstable"
+      "ipu6-camera-bins"
+      "ivsc-firmware-unstable"
+      "ivsc-firmware"
+    ];
 
   environment.systemPackages = with pkgs; [
     zoom-us
@@ -169,5 +200,20 @@
     ACTION=="remove",\
       ENV{SUBSYSTEM}=="drm",\
       RUN+="${pkgs.autorandr}/bin/autorandr --batch --change"
+
+    SUBSYSTEM!="video4linux", GOTO="hide_cam_end"
+    ATTR{name}!="Dummy video device (0x0000)", GOTO="hide_cam_end"
+    ACTION=="add", RUN+="${pkgs.coreutils}/bin/mkdir -p /dev/not-for-user"
+    ACTION=="add", RUN+="${pkgs.coreutils}/bin/mv -f $env{DEVNAME} /dev/not-for-user/"
+    ACTION=="remove", RUN+="${pkgs.coreutils}/bin/rm -f /dev/not-for-user/$name"
+    ACTION=="remove", RUN+="${pkgs.coreutils}/bin/rm -f /dev/not-for-user/$env{ID_SERIAL}"
+    LABEL="hide_cam_end"
   '';
+
+  hardware.ipu6 = {
+    enable = true;
+    platform = "ipu6ep";
+  };
+  hardware.enableRedistributableFirmware = true;
+  hardware.firmware = [ ivsc-firmware ];
 }
