f81d7ee28c121f131d3b76814271a011de06c4be
Christoph Blessing <chris24.blessing@gmail.com>
2024-06-21 12:06:29 +0200

    Add more nu completions

--- shared/nushell/default.nix
+++ shared/nushell/default.nix
@@ -12,6 +12,9 @@
       use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/git/git-completions.nu *
       use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/zellij/zellij-completions.nu *
       use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/nix/nix-completions.nu *
+      use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/rg/rg-completions.nu *
+      use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/man/man-completions.nu *
+      use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/docker/docker-completions.nu *
     '';
     envFile.source = ./env.nu;
   };
