7855da870f29cc81631ac759baeae954c7dff42e
Christoph Blessing <chris24.blessing@gmail.com>
2024-10-05 22:50:25 +0200

    Pause dunst while pymodoro is active

--- work/pymodoro/pymodoro.nix
+++ work/pymodoro/pymodoro.nix
@@ -1,11 +1,35 @@
-{ pkgs, pymodoro, ... }:
+{
+  pkgs,
+  lib,
+  pymodoro,
+  ...
+}:
 let
   pymodoroPkg = pymodoro.packages.${pkgs.system}.default;
+  dunstPause = pkgs.writeShellScriptBin "dunstPause" ''
+    PATH=${lib.makeBinPath [ pkgs.dunst ]}
+
+    if [[ "$1" == 'pause' ]]; then
+      message='Notifications paused'
+      is_paused='true'
+    elif [[ "$1" == 'unpause' ]]; then
+      message='Notifications unpaused'
+      is_paused='false'
+    else
+      echo "Unknown parameter '$1'"
+      exit 1
+    fi
+
+    dunstify --appname="pymodoro" "$message"
+    dunstctl set-paused "$is_paused"
+  '';
 in
 {
   home.packages = [ pymodoroPkg ];
   home.file.".config/pymodoro/config.toml".text = ''
     [pymodorod]
+    begin_cmd = ["${dunstPause}/bin/dunstPause", "pause"]
+    end_cmd = ["${dunstPause}/bin/dunstPause", "unpause"]
     done_cmd = ["${pkgs.alsa-utils}/bin/aplay", "${./school-bell.wav}"]
 
     [pymodoro]
@@ -22,6 +46,12 @@ in
       Install = {
         WantedBy = [ "multi-user.target" ];
       };
+    };
+  };
+  services.dunst.settings = {
+    allow_pymodoro = {
+      appname = "pymodoro";
+      override_pause_level = 100;
     };
   };
 }
