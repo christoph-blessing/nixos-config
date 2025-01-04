e106fd2d35e93c3f1313838f0ae893529f989818
Christoph Blessing <chris24.blessing@gmail.com>
2025-01-04 22:55:52 +0100

    Add mic mute keybind

--- work/home.nix
+++ work/home.nix
@@ -151,6 +151,7 @@
       "XF86AudioMute" = "${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
       "XF86AudioLowerVolume" = "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
       "XF86AudioRaiseVolume" = "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
+      "XF86AudioMicMute" = "${pkgs.pulseaudio}/bin/pactl set-source-mute @DEFAULT_SOURCE@ toggle";
     };
   };
 
