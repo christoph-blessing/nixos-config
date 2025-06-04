162dfaa202419637baae5d7e27bead8e656ea6aa
Christoph Blessing <chris24.blessing@gmail.com>
2025-06-04 15:13:35 +0200

    Update kernel to 6.15

--- work/configuration.nix
+++ work/configuration.nix
@@ -7,7 +7,7 @@
     ./mitmproxy.nix
   ];
 
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_13;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_15;
   boot.initrd = {
     kernelModules = [
       "vfat"
