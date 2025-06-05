60504988da8d0d7a65714631878e8c15aea71749
Christoph Blessing <chris24.blessing@gmail.com>
2025-06-05 16:12:34 +0200

    Update nvim lsp

--- shared/home.nix
+++ shared/home.nix
@@ -155,6 +155,7 @@
     nixd
     fd
     zls
+    gopls
   ];
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
--- shared/neovim/init.lua
+++ shared/neovim/init.lua
@@ -142,25 +142,61 @@ vim.api.nvim_create_autocmd("LspAttach", {
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-local lspconfig = require("lspconfig")
-lspconfig.lua_ls.setup({
-	settings = {
-		Lua = {
-			completion = {
-				callSnippet = "Replace",
+local language_servers = {
+	{
+		name = "lua_ls",
+		config = {
+			on_init = function(client)
+				if client.workspace_folders then
+					local path = client.workspace_folders[1].name
+					if
+						path ~= vim.fn.stdpath("config")
+						and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+					then
+						return
+					end
+				end
+
+				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+					runtime = {
+						version = "LuaJIT",
+						path = {
+							"lua/?.lua",
+							"lua/?/init.lua",
+						},
+					},
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+						},
+					},
+				})
+			end,
+			settings = {
+				Lua = {
+					completion = {
+						callSnippet = "Replace",
+					},
+					diagnostics = { globals = { "vim" } },
+				},
 			},
-			diagnostics = { globals = { "vim" } },
 		},
 	},
-	capabilities = capabilities,
-})
-lspconfig.pyright.setup({
-	settings = {
-		typeCheckingMode = "off",
-	},
-})
-lspconfig.nixd.setup({})
-lspconfig.zls.setup({})
+	{ name = "pyright", config = {
+		settings = {
+			typeCheckingMode = "off",
+		},
+	} },
+	{ name = "nixd", config = {} },
+	{ name = "zls", config = {} },
+	{ name = "gopls", config = {} },
+}
+for i = 1, #language_servers do
+	local server = language_servers[i]
+	vim.lsp.enable(server.name)
+	vim.lsp.config(server.name, server.config)
+end
 
 require("conform").setup({
 	notify_on_error = false,
