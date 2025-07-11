f3041fef5041e0ec6ce99c1e90ae10cc6749fe64
Christoph Blessing <chris24.blessing@gmail.com>
2025-07-11 14:19:43 +0200

    Use main nu_scripts instead of fork

--- shared/nushell/default.nix
+++ shared/nushell/default.nix
@@ -8,25 +8,14 @@
   programs.nushell = {
     enable = true;
     configFile.source = ./config.nu;
-    extraConfig =
-      let
-        my_nu_scripts = pkgs.nu_scripts.overrideAttrs (old: {
-          src = pkgs.fetchFromGitHub {
-            owner = "christoph-blessing";
-            repo = old.src.repo;
-            rev = "f6fd4ae6f56122d01e378ef1a618e5cb898d1bdb";
-            hash = "sha256-eVPWCfQdleV+bVoGQMS6tNVFHHexakj9M6cO4/w1p8g=";
-          };
-        });
-      in
-      ''
-        use ${my_nu_scripts}/share/nu_scripts/custom-completions/git/git-completions.nu *
-        use ${my_nu_scripts}/share/nu_scripts/custom-completions/zellij/zellij-completions.nu *
-        use ${my_nu_scripts}/share/nu_scripts/custom-completions/nix/nix-completions.nu *
-        use ${my_nu_scripts}/share/nu_scripts/custom-completions/rg/rg-completions.nu *
-        use ${my_nu_scripts}/share/nu_scripts/custom-completions/man/man-completions.nu *
-        use ${my_nu_scripts}/share/nu_scripts/custom-completions/docker/docker-completions.nu *
-      '';
+    extraConfig = with pkgs; ''
+      use ${nu_scripts}/share/nu_scripts/custom-completions/git/git-completions.nu *
+      use ${nu_scripts}/share/nu_scripts/custom-completions/zellij/zellij-completions.nu *
+      use ${nu_scripts}/share/nu_scripts/custom-completions/nix/nix-completions.nu *
+      use ${nu_scripts}/share/nu_scripts/custom-completions/rg/rg-completions.nu *
+      use ${nu_scripts}/share/nu_scripts/custom-completions/man/man-completions.nu *
+      use ${nu_scripts}/share/nu_scripts/custom-completions/docker/docker-completions.nu *
+    '';
     envFile.source = ./env.nu;
   };
 }
