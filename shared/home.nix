eb2817ef5f4018973aad46aedf0131e8edeaa2dd
Christoph Blessing <chris24.blessing@gmail.com>
2025-10-24 15:44:57 +0200

    Update hyprland/idle configuration

--- shared/home.nix
+++ shared/home.nix
@@ -127,9 +127,9 @@
       exec-once = with pkgs; [
         "[workspace 1] ${alacritty}/bin/alacritty"
         "[workspace 2 silent] ${firefox}/bin/firefox -P default"
-        "[workspace 3 silent] ${keepassxc}/bin/keepassxc"
-        "[workspace 4 silent] ${firefox}/bin/firefox -P element"
-        "[workspace 5 silent] ${firefox}/bin/firefox -P perplexity"
+        "[workspace 3 silent] ${firefox}/bin/firefox -P perplexity"
+        "[workspace 4 silent] ${keepassxc}/bin/keepassxc"
+        "[workspace 5 silent] ${firefox}/bin/firefox -P element"
         "${kanshi}/bin/kanshi"
         "${killall}/bin/killall waybar; waybar"
         "${zellij}/bin/zellij kill-all-sessions --yes"
@@ -204,9 +204,9 @@
       workspace = [
         "1,defaultName:terminal"
         "2,defaultName:web"
-        "3,defaultName:password"
-        "4,defaultName:messenger"
-        "5,defaultName:ai"
+        "3,defaultName:ai"
+        "4,defaultName:password"
+        "5,defaultName:messenger"
       ];
       windowrule = [
         "suppressevent maximize, class:.*"
@@ -222,22 +222,50 @@
       general = {
         lock_cmd = "${pkgs.toybox}/bin/pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
       };
-      listener = [
-        {
-          timeout = 300;
-          on-timeout = "${pkgs.systemd}/bin/loginctl lock-session";
-        }
-        {
-          timeout = 270;
-          on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10";
-          on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";
-        }
-        {
-          timeout = 330;
-          on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
-          on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on && ${pkgs.brightnessctl}/bin/brightnessctl -r";
-        }
-      ];
+      listener =
+        let
+          displayctl = pkgs.writeShellScriptBin "displayctl" ''
+            PATH=${
+              lib.makeBinPath [
+                pkgs.coreutils
+                pkgs.hyprland
+                pkgs.brightnessctl
+              ]
+            }
+
+            should_be_plugged_in=$1
+            action=$2
+
+            if [ $(${pkgs.coreutils}/bin/cat /sys/class/power_supply/AC/online) -eq "$should_be_plugged_in" ]; then
+              if [ "$action" = "timeout" ]; then
+                hyprctl dispatch dpms off
+              else
+                hyprctl dispatch dpms on && brightnessctl -r
+              fi
+            fi
+          '';
+        in
+        [
+          {
+            timeout = 270;
+            on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10";
+            on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";
+          }
+          {
+            timeout = 300;
+            on-timeout = "${pkgs.systemd}/bin/loginctl lock-session";
+          }
+          {
+            timeout = 330;
+            on-timeout = "${displayctl}/bin/displayctl 0 timeout";
+            on-resume = "${displayctl}/bin/displayctl 0 resume";
+          }
+          {
+            timeout = 600;
+            on-timeout = "${displayctl}/bin/displayctl 1 timeout";
+            on-resume = "${displayctl}/bin/displayctl 1 resume";
+          }
+        ];
     };
   };
 
