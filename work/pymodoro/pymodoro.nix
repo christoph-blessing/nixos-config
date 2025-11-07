5333402e0b953f63e9d044392682df5801d7a740
Christoph Blessing <chris24.blessing@gmail.com>
2025-11-07 14:22:25 +0100

    Display message at end of pymodoro

--- work/pymodoro/pymodoro.nix
+++ work/pymodoro/pymodoro.nix
@@ -6,7 +6,7 @@
 }:
 let
   pymodoroPkg = pymodoro.packages.${pkgs.system}.default;
-  dunstPause = pkgs.writeShellScriptBin "dunstPause" ''
+  dunstPause = pkgs.writeShellScriptBin "dunst-pause" ''
     PATH=${lib.makeBinPath [ pkgs.dunst ]}
 
     if [[ "$1" == 'pause' ]]; then
@@ -23,22 +23,35 @@ let
     dunstify --appname="pymodoro" "$message"
     dunstctl set-paused "$is_paused"
   '';
-in
-{
-  home.packages = [ pymodoroPkg ];
-  home.file.".config/pymodoro/config.toml".text = ''
+  pymodoroNotify = pkgs.writeShellScriptBin "pd-notify" ''
+    PATH=${
+      lib.makeBinPath [
+        pkgs.alsa-utils
+        pkgs.dunst
+      ]
+    }
+
+    dunstify --appname="pymodoro" --urgency="critical" 'Take a break!'
+    aplay '${./school-bell.wav}'
+  '';
+  configContents = ''
     [pymodorod]
-    begin_cmd = ["${dunstPause}/bin/dunstPause", "pause"]
-    end_cmd = ["${dunstPause}/bin/dunstPause", "unpause"]
-    done_cmd = ["${pkgs.alsa-utils}/bin/aplay", "${./school-bell.wav}"]
+    begin_cmd = ["${dunstPause}/bin/dunst-pause", "pause"]
+    end_cmd = ["${dunstPause}/bin/dunst-pause", "unpause"]
+    done_cmd = ["${pymodoroNotify}/bin/pd-notify"]
 
     [pymodoro]
     default_duration = "25m"
   '';
+in
+{
+  home.packages = [ pymodoroPkg ];
+  home.file.".config/pymodoro/config.toml".text = configContents;
   systemd.user.services = {
     pymodoro = {
       Unit = {
         Description = "Pymodoro timer daemon";
+        X-Restart-Triggers = [ (builtins.hashString "sha256" configContents) ];
       };
       Service = {
         ExecStart = "${pymodoroPkg}/bin/pymodorod";
