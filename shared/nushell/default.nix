1b799b38e9bd10d80402aeb9190b5ccb0c9718da
Christoph Blessing <chris24.blessing@gmail.com>
2026-05-06 14:57:06 +0200

    remove nu scripts completions

--- shared/nushell/default.nix
+++ shared/nushell/default.nix
@@ -1,16 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.nushell = {
     enable = true;
     configFile.source = ./config.nu;
-    extraConfig = with pkgs; ''
-      use ${nu_scripts}/share/nu_scripts/custom-completions/git/git-completions.nu *
-      use ${nu_scripts}/share/nu_scripts/custom-completions/zellij/zellij-completions.nu *
-      use ${nu_scripts}/share/nu_scripts/custom-completions/nix/nix-completions.nu *
-      use ${nu_scripts}/share/nu_scripts/custom-completions/rg/rg-completions.nu *
-      use ${nu_scripts}/share/nu_scripts/custom-completions/man/man-completions.nu *
-      use ${nu_scripts}/share/nu_scripts/custom-completions/docker/docker-completions.nu *
-    '';
     envFile.source = ./env.nu;
   };
 
