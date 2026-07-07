4006dca2d720cf9c46a35e2324424e26403eb6ce
Christoph Blessing <chris24.blessing@gmail.com>
2026-07-07 15:16:34 +0200

    add keymap for rust lsp code action

--- shared/neovim/after/ftplugin/rust.lua
+++ shared/neovim/after/ftplugin/rust.lua
@@ -7,3 +7,7 @@ end, { buffer = bufnr, desc = "Rendered rustc diagnostic" })
 vim.keymap.set("n", "<leader>x", function()
 	vim.cmd.RustLsp("explainError")
 end, { buffer = bufnr, desc = "rustc --explain" })
+
+vim.keymap.set("n", "gra", function()
+	vim.cmd.RustLsp("codeAction")
+end, { buffer = bufnr, desc = "Code action (grouped)" })
