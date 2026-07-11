132a7e46cfb69c72a3871a0cb61878e865c97f65
Christoph Blessing <chris24.blessing@gmail.com>
2026-07-11 16:41:36 +0200

    update rust ftplugin

--- shared/neovim/after/ftplugin/rust.lua
+++ shared/neovim/after/ftplugin/rust.lua
@@ -1,8 +1,16 @@
 local bufnr = vim.api.nvim_get_current_buf()
 
 vim.keymap.set("n", "<leader>e", function()
-	vim.cmd.RustLsp("renderDiagnostic")
+	vim.cmd.RustLsp("renderDiagnostic", "current")
 end, { buffer = bufnr, desc = "Rendered rustc diagnostic" })
+
+vim.keymap.set("n", "]d", function()
+	vim.cmd.RustLsp("renderDiagnostic", "cycle")
+end, { buffer = bufnr, desc = "Go to next [D]iagnostic message" })
+
+vim.keymap.set("n", "[d", function()
+	vim.cmd.RustLsp("renderDiagnostic", "cycle_prev")
+end, { buffer = bufnr, desc = "Go to previous [D]iagnostic message" })
 
 vim.keymap.set("n", "<leader>x", function()
 	vim.cmd.RustLsp("explainError")
