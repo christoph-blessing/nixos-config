7970fe64dbb25ca18412619163047acc84cfd6ab
Christoph Blessing <chris24.blessing@gmail.com>
2026-07-20 11:22:20 +0200

    add ruff_fix to confrom.nvim

--- shared/neovim/lua/plugins.lua
+++ shared/neovim/lua/plugins.lua
@@ -323,7 +323,7 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				nix = { "nixfmt" },
-				python = { "ruff_format", "ruff_organize_imports" },
+				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
 				go = { "gofmt" },
 				htmldjango = { "djlint" },
 				yaml = { "yamlfmt" },
